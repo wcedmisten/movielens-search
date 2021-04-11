0403a4ea17274e8281db08e63c0b5794d00b22eb
William Edmisten <wcedmisten@gmail.com>
2021-04-10 21:59:09 -0400

    Initialize database from csv dataset

--- db/sql/init.sql
+++ db/sql/init.sql
@@ -1,5 +1,21 @@
-USE challenge;
+CREATE TABLE movies (id int primary key, title varchar, genres varchar);
+\copy movies FROM '/dataset/movies.csv' delimiter ',' CSV HEADER ;
 
-CREATE TABLE test(col VARCHAR(10));
+CREATE TABLE ratings (user_id int, movie_id int, rating decimal, epoch_timestamp int,
+    CONSTRAINT fk_movie_id FOREIGN KEY(movie_id) REFERENCES movies(id)
+);
 
-INSERT INTO test(col) VALUES('ok');
+\copy ratings FROM '/dataset/ratings.csv' delimiter ',' CSV HEADER ;
+
+CREATE TABLE tags (user_id int, movie_id int, tag varchar, epoch_timestamp int,
+    CONSTRAINT fk_movie_id FOREIGN KEY(movie_id) REFERENCES movies(id)
+);
+\copy tags FROM '/dataset/tags.csv' delimiter ',' CSV HEADER ;
+
+CREATE TABLE links (movie_id int, imdb_id int, tmdb_id int,
+    CONSTRAINT fk_movie_id FOREIGN KEY(movie_id) REFERENCES movies(id)
+);
+\copy links FROM '/dataset/links.csv' delimiter ',' CSV HEADER ;
+
+/* update the trigram extension for search via similiarity() */
+create extension pg_trgm;
